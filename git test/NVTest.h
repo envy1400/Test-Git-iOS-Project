dff4094935469678ea0f1dd08a7d437bbbbda6f1
envy1400 <nv1400@gmail.com>
2014-09-07 21:06:43 -0500

    added a test class

---
+++ git test/NVTest.h
@@ -0,0 +1,13 @@
+//
+//  NVTest.h
+//  git test
+//
+//  Created by Nick Vinyard on 9/7/14.
+//  Copyright (c) 2014 Nick Vinyard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NVTest : NSObject
+
+@end
---
+++ git test/NVTest.m
@@ -0,0 +1,13 @@
+//
+//  NVTest.m
+//  git test
+//
+//  Created by Nick Vinyard on 9/7/14.
+//  Copyright (c) 2014 Nick Vinyard. All rights reserved.
+//
+
+#import "NVTest.h"
+
+@implementation NVTest
+
+@end
